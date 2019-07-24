b0c6b2c8ff2c03fd427f270c0843babf887eb09c
hyeon62 <hyeon.rot@gmail.com>
2019-07-24 23:52:23 +0900

    Divide to store;

--- mobx-with-react/src/Counter.js
+++ mobx-with-react/src/Counter.js
@@ -1,36 +1,26 @@
 import React, { Component } from 'react';
-import { decorate, observable, action } from 'mobx';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 
+// **** 함수형태로 파라미터를 전달해주면 특정 값만 받아올 수 있음.
+// @inject(stores => ({
+//   number: stores.counter.number,
+//   increase: stores.counter.increase,
+//   decrease: stores.counter.decrease,
+// }))
+@inject('counter')  // ! check
 @observer
 class Counter extends Component {
-  @observable number = 0;
-  
-  @action
-  increase = () => {
-    this.number++;
-  }
-
-  @action
-  decrease = () => {
-    this.number--;
-  }
-
   render() {
+    const { counter } = this.props;
     return (
       <div>
-        <h1>{this.number}</h1>
-        <button onClick={this.increase}>+1</button>
-        <button onClick={this.decrease}>-1</button>
+        <h1>{counter.number}</h1>
+        <button onClick={counter.increase}>+1</button>
+        <button onClick={counter.decrease}>-1</button>
       </div>
     );
   }
 }
 
-decorate(Counter, {
-  number: observable,
-  increase: action,
-  decrease: action
-})
 
 export default Counter;
