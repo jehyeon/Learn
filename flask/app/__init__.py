50160b948fea4222ce3230f43802f39f7156383b
hyeon62 <hyeon.rot@gmail.com>
2019-07-30 18:08:38 +0900

    Add comments

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 
 from app.main.index import main as main
 
+# ? What is flaskApp.register_blueprint 
 app.register_blueprint(main)
--- flask/app/main/index.py
+++ flask/app/main/index.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for
 from flask import current_app as current_app
 
+# ? What is Blueprint 
 main = Blueprint('main', __name__, url_prefix='/')
 
 @main.route('/main', methods=['GET'])
