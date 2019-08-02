b5b41f9465ae3483e6872ebc774b2ea2e6a779fa
hyeon62 <hyeon.rot@gmail.com>
2019-08-02 17:05:03 +0900

    Init

---
+++ electron/mama/renderer.js
@@ -0,0 +1,26 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const { ipcRenderer } = require('electron');
+
+document.getElementById('todoForm').addEventListener('submit', evt => {
+    // Prevent default refresh functionality of forms
+    evt.preventDefault();
+
+    // Input on the form
+    const input = evt.target[0];
+
+    // Send todo to main process
+    ipcRenderer.send('add-todo', input.value);
+
+    // Reset input
+    input.value = '';
+});
+
+
+// Functions
+
+// Delete todo by its text value
+const deleteTodo = e => {
+    ipcRenderer.send('delete-todo', e.target.textContent);
+};
